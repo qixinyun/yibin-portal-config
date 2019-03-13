54f5a59764933c7ea6da97d0bac39949f9559628
WooKy <939410146@qq.com>
2019-03-13 16:08:56 +0800

    #0.1.0

--- src/SmartyConfig/Common/SmartyConfig.php
+++ src/SmartyConfig/Common/SmartyConfig.php
@@ -6,9 +6,9 @@ class SmartyConfig
     public static function configDir()
     {
         $dirArray = array(
-            S_ROOT.'vendor/qixinyun/pingxiang-portal-config/src/SmartyConfig/Yb',
-            S_ROOT.'vendor/qixinyun/pingxiang-portal-config/src/SmartyConfig/Common',
-            S_ROOT.'vendor/qixinyun/pingxiang-portal-config/src/SmartyConfig/',
+            S_ROOT.'vendor/qixinyun/yibin-portal-config/src/SmartyConfig/Yb',
+            S_ROOT.'vendor/qixinyun/yibin-portal-config/src/SmartyConfig/Common',
+            S_ROOT.'vendor/qixinyun/yibin-portal-config/src/SmartyConfig/',
         );
 
         return $dirArray;
